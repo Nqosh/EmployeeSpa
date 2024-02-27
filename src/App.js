17def931c6a690dd3da3a4242d72f70398369ed3
Nqobile Moyo <NqobileM@ssda.co.za>
2024-02-27 07:14:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+import EmployeeCrud from './components/EmployeeCrud';
+
+function App() {
+  return (
+    <div className="App">
+    <EmployeeCrud></EmployeeCrud>
+    </div>
+  );
+}
+
+export default App;
